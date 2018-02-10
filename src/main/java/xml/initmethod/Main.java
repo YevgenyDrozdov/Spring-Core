6aa263fac56f565786395ef32e122adecea98394
ydrozdov <yevgeny_drozdov1@epam.com>
2018-02-11 01:44:42 +0300

    Small change

--- src/main/java/xml/initmethod/Main.java
+++ src/main/java/xml/initmethod/Main.java
@@ -8,6 +8,8 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("init-method-context.xml");
         //context.getBean(Human.class);
+
+        System.out.println(Integer.MAX_VALUE);
     }
 
 }
